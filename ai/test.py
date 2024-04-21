e2243a08adbef1456f56a8d26e6f052f58b1b8cb
sidtuladhar <smt9598@nyu.edu>
2024-04-21 10:07:08 -0400

    improved AI

--- ai/test.py
+++ ai/test.py
@@ -21,7 +21,7 @@ from langchain_openai import OpenAIEmbeddings
 load_dotenv()
 
 db = SQLDatabase.from_uri("sqlite:///mydatabase.sqlite")
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
 
 examples = [
     {"input": "How many professors are there?", 
@@ -47,19 +47,19 @@ examples = [
     "query": "SELECT c.course_title, i.name FROM courses c JOIN course_instructor ci ON c.crn = ci.course_id JOIN instructors i ON ci.instructor_id = i.id ORDER BY c.course_title"
     },
     {
-    "input": "List all courses that are about 'data analysis'.",
+    "input": "List all courses that are related to data analysis at night",
     "query": "SELECT course_title, description FROM courses WHERE description LIKE '%data analysis%'"
     },
     {
-    "input": "Can you tell me some courses that have 'machine learning' and 'artificial intelligence'?",
+    "input": "Can you tell me some courses that are about machine learning and artificial intelligence?",
     "query": "SELECT crn, subject_code, course_number, course_title, description FROM courses WHERE description LIKE '%machine learning%' AND description LIKE '%artificial intelligence%'"
     },
     {
-    "input": "I don't want to wake up early. Recommend some finance courses.",
+    "input": "I don't want to wake up early. Recommend some finance courses",
     "query": "SELECT crn, subject_code, course_number, course_title, start_time, description FROM courses WHERE subject_code = 'FIN' AND start_time >= '10:00'"
     },
     {
-    "input": "Give me some high rated architecture professors and the courses they teach.",
+    "input": "Give me some high rated architecture professors and the courses they teach",
     "query": "SELECT i.name AS Professor_Name, i.avg_rating AS Rating, c.course_title AS Course FROM instructors i JOIN course_instructor ci ON i.id = ci.instructor_id JOIN courses c ON ci.course_id = c.crn WHERE i.avg_rating >= 4 AND c.subject_code = 'ARCH'"
     },
     {
@@ -71,14 +71,21 @@ examples = [
     "query": "SELECT c.crn, c.subject_code, c.course_number, c.course_title, i.avg_rating FROM courses c JOIN course_instructor ci ON c.crn = ci.course_id JOIN instructors i ON ci.instructor_id = i.id WHERE i.avg_rating >= 4 AND (c.prereqs IS NULL OR c.prereqs = '')"
     },
     {
-    "input": "Find math courses available on Tuesday and Thursday between 2pm and 5pm.",
+    "input": "Find math courses available on Tuesday and Thursday between 2pm and 5pm",
     "query": "SELECT crn, subject_code, course_number, course_title, start_time, end_time, days FROM courses WHERE subject_code = 'MATH' AND days LIKE '%Tu%' AND days LIKE '%Th%' AND TIME(start_time) >= TIME('14:00') AND TIME(end_time) <= TIME('17:00')"
     },
     {
-    "input": "Find me an online elective that is on Friday.",
+    "input": "Find me an online elective that is on Friday",
     "query": "SELECT crn, subject_code, course_number, course_title, days FROM courses WHERE instruction_method = 'Online-Asynchronous' AND days LIKE '%Friday%' AND (prereqs IS NULL OR prereqs = '')"
+    },
+    {
+    "input": "give me electives that are related to cooking that have high ratings",
+    "query": "SELECT c.crn, c.subject_code, c.course_number,c.course_title, i.avg_rating, FROM courses c JOIN course_instructor ci ON c.crn = ci.course_id JOIN instructors i ON ci.instructor_id = i.id WHERE c.description LIKE '%cooking%' AND (c.prereqs IS NULL OR c.prereqs = '') AND i.avg_rating >= 4"
+    },
+    {
+    "input": "What class does Professor Mark teach?",
+    "query": "SELECT c.crn, c.subject_code, c.course_title, i.name FROM courses c JOIN course_instructor ci ON c.crn = ci.course_id JOIN instructors i ON ci.instructor_id = i.id WHERE i.name LIKE '%Mark%'"
     }
-
 
 ]
 
@@ -103,7 +110,7 @@ DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the databa
 
 If the question does not seem related to the database, just return "I don't know" as the answer.
 
-While listing courses, ALWAYS include the course title, course number, and subject code in the format (crn: subject_codecourse_number,) LIKE (11326: CS160).
+While listing courses, ALWAYS include the course title, course number, and subject code in the format (subject_codecourse_number).
 
 Here are some examples of user inputs and their corresponding SQL queries:"""
 
@@ -158,7 +165,7 @@ if __name__ == "__main__":
     agent_type="openai-tools",
     )
 
-    print(agent.invoke({"input": "give me some online finance courses",
+    print(agent.invoke({"input": "What class does professor Mark teach?",
                         "top_k": 5,
                         "dialect": "SQLite",
                         "agent_scratchpad": []}))
