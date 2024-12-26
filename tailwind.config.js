b115728a71757b045d7b55c9e620c35e6bc966e7
AbyssArchitect <justyntime2024@gmail.com>
2024-12-26 02:56:54 -0600

    Massive refactorization of tree code and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     extend: {},
   },
   plugins: [
-      require("daisyui")
+    require("daisyui")
   ],
+  daisyui: {
+    themes: [
+      {
+        dark_drexel: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          "primary": "#0D2C54",
+          "secondary": "yellow",
+          "accent": "lightblue"
+        },
+        light_drexel: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#0D2C54",
+          "secondary": "yellow",
+          "accent": "lightblue"
+        },
+      },
+      "light",
+      "dark"
+    ]
+  }
 }
 
